bacc0405bc3725d32024a59a4ad32f960c33db13
Cameron Bytheway <bytheway.cameron@gmail.com>
2017-02-12 01:21:45 -0500

    how did tabs get in there?

--- js/rollup.compiler.js
+++ js/rollup.compiler.js
@@ -1,16 +1,16 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
-	entry: 'lib/es6/src/lib/statechart.js',
-	plugins: [
-		nodeResolve({
-			jsnext: true
-		}),
-	],
-	sourceMap: true,
-	moduleName: 'statechart-compiler',
-	targets: [
-		{ dest: 'js/lib/compiler.js', format: 'cjs' },
-		{ dest: 'js/src/compiler.js', format: 'es' }
-	]
+  entry: 'lib/es6/src/lib/statechart.js',
+  plugins: [
+    nodeResolve({
+      jsnext: true
+    }),
+  ],
+  sourceMap: true,
+  moduleName: 'statechart-compiler',
+  targets: [
+    { dest: 'js/lib/compiler.js', format: 'cjs' },
+    { dest: 'js/src/compiler.js', format: 'es' }
+  ]
 };
--- js/rollup.config.js
+++ js/rollup.config.js
@@ -2,20 +2,20 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import multiEntry from 'rollup-plugin-multi-entry';
 
 export default {
-	entry: [
-		'lib/es6/src/lib/statechart.js',
-		'lib/es6/src/engine/js/interpreter.js'
-	],
-	plugins: [
-		nodeResolve({
-			jsnext: true
-		}),
-		multiEntry()
-	],
-	sourceMap: true,
-	moduleName: 'statechart',
-	targets: [
-		{ dest: 'js/lib/index.js', format: 'cjs' },
-		{ dest: 'js/src/index.js', format: 'es' }
-	]
+  entry: [
+    'lib/es6/src/lib/statechart.js',
+    'lib/es6/src/engine/js/interpreter.js'
+  ],
+  plugins: [
+    nodeResolve({
+      jsnext: true
+    }),
+    multiEntry()
+  ],
+  sourceMap: true,
+  moduleName: 'statechart',
+  targets: [
+    { dest: 'js/lib/index.js', format: 'cjs' },
+    { dest: 'js/src/index.js', format: 'es' }
+  ]
 };
